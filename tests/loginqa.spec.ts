9ea39b6559cd7d0e02c78d8835068fd9ab88978c
arceclaudioandres <143361214+arceclaudioandres@users.noreply.github.com>
2025-09-03 14:17:07 +0000

    updated comments in code

--- tests/loginqa.spec.ts
+++ tests/loginqa.spec.ts
@@ -1,33 +1,33 @@
 import { test, expect } from '@playwright/test';
 
 test('login works correctly with Identity Provider', async ({ page }) => {
-  // 1. Ir al home
+  // Ir al home
   await page.goto('https://agri-ar-v2-qa.pilkington.com/');
 
-  // 2. Esperar redirección al login externo (regex para que funcione aunque cambie el hash)
+  // Esperar redirección al login externo (regex para que funcione aunque cambie el hash)
   await page.waitForURL(/https:\/\/identity-qa\.pilkington\.com\/identityexternal\/login.*/);
 
-  // 3. Completar login
+  // Completar login
   await page.fill('input#username', 'ARCECLAUDIOCCQA');
   await page.fill('input#password', 'Tlwn722g#');
 
-  // 4. Marcar Terms and Conditions
+  // Marcar Terms and Conditions
   await page.getByLabel('I agree to the Terms and Conditions').click();
 
 
-  // 5. Guardar URL antes de hacer login
+  // Guardar URL antes de hacer login
   const urlBefore = page.url();
 
-  // 6. Click en Sign In y esperar navegación
+  // Click en Sign In y esperar navegación
   // Click Sign In
   await page.screenshot({ path: 'login-bclick.png', fullPage: true });
   await page.click('button:has-text("Sign In")');
   await page.waitForURL('https://agri-ar-v2-qa.pilkington.com/', { timeout: 60000 });
 
-  // 7. Guardar URL después del login
+  // Guardar URL después del login
   const urlAfter = page.url();
 
-  // 8. Validar que cambió la URL
+  // Validar que cambió la URL
   expect(urlAfter).not.toBe(urlBefore);
 
 
@@ -41,13 +41,13 @@ test('login works correctly with Identity Provider', async ({ page }) => {
   const claimsHeader = page.locator('label:has-text("Claims Report")'); // Ajustar según el HTML
   await expect(claimsHeader).toBeVisible({ timeout: 60000 });
 
-  // 1️⃣ Localizar todos los items de claims
+  // Localizar todos los items de claims
   const claimItems = page.locator('claim-report-item');
 
-  // 2️⃣ Esperar a que al menos uno sea visible
+  // Esperar a que al menos uno sea visible
   await expect(claimItems.first()).toBeVisible({ timeout: 60000 });
 
-  // 3️⃣ Opcional: validar que hay al menos un item
+  // Opcional: validar que hay al menos un item
   const count = await claimItems.count();
   expect(count).toBeGreaterThan(0);
 
